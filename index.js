fcadd5b2f68739522a93757e6fffdef0ac758c1f
DongHeon Lee <niche3600@gmail.com>
2021-12-12 18:48:14 +0900

    ビルドスクリプト改善

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+const path = require("path");
+const fs = require("fs");
 const PDFMerger = require('pdf-merger-js');
 
 var merger = new PDFMerger();
 
-(async () => {
-  merger.add('./output/1.pdf');
-  merger.add('./output/2.pdf');
+const OUTPUT_PATH = './output';
+const RESULT_PDF_PATH = './pdf/book.pdf';
 
-  await merger.save('./pdf/book2.pdf');
+const outputPdfs = fs.readdirSync(OUTPUT_PATH)
+  .filter(file => {
+    return fs.statSync(path.join(OUTPUT_PATH, file)).isFile()
+      && /.*\.pdf$/.test(file)
+  })
+  .map(fileName => {
+    return path.join(OUTPUT_PATH, fileName)
+  });
+
+const mergerAdd = () => {
+  outputPdfs.map(filePath => { merger.add(filePath) });
+};
+
+(async () => {
+  mergerAdd();
+
+  await merger.save(RESULT_PDF_PATH);
 })()
