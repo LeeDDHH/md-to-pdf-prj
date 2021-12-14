5426ea0aa1d558137cf8b45c71a32e80dec88edb
DongHeon Lee <niche3600@gmail.com>
2021-12-14 14:26:49 +0900

    contents配下の構成修正、出力フォーマットを設定

--- config.js
+++ config.js
@@ -2,5 +2,10 @@ module.exports = {
   stylesheet: ['https://fonts.googleapis.com/css?family=Zen+Maru+Gothic'],
   css: `body { font-family: "Zen Maru Gothic"; }`,
   body_class: 'markdown-body',
-  stylesheet_encoding: 'utf-8'
+  stylesheet_encoding: 'utf-8',
+  pdf_options: {
+		format: 'A4',
+		margin: '10mm',
+		printBackground: true,
+	}
 }
