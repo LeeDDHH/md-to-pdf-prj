a35f2d744c96502e6c48d32344359fb98a721be6
DongHeon Lee <niche3600@gmail.com>
2021-12-14 12:56:34 +0900

    configファイルでスタイル設定を管理

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  stylesheet: ['https://fonts.googleapis.com/css?family=Zen+Maru+Gothic'],
+  css: `body { font-family: "Zen Maru Gothic"; }`,
+  body_class: 'markdown-body',
+  stylesheet_encoding: 'utf-8'
+}
